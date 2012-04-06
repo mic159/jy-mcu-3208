a6dcedf85d5c1342dd565bf52fd7e0c5f6a4f253
Michael Cooper <mic159@gmail.com>
2012-04-06 23:01:22 +1000

    Use SerialRunner

--- src/demo.cpp
+++ src/demo.cpp
@@ -1,17 +1,17 @@
 #include "display.h"
 #include "effects/hscroll.h"
 #include "runner.h"
-#include "scriptrunner.h"
+#include "serialrunner.h"
 #include "fonts/jiffy.h"
 
 int main(void) {
 
     Display display;
     Runner runner(display);
-    ScriptRunner scriptRunner(runner);
+    SerialRunner serialRunner(runner);
 
     while (1)
-        scriptRunner.run();
+        serialRunner.run();
 
     return(0);
 }
