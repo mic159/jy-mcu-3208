cb480a9660c91da0f8160cc2575553f70b91c4ae
Michael Cooper <mic159@gmail.com>
2012-04-08 00:41:56 +1000

    More work on test controller

--- control/main.cpp
+++ control/main.cpp
@@ -3,37 +3,94 @@
 #include <iostream>
 #include <termios.h> // POSIX terminal control definitions
 #include <stdint.h>
+#include <unistd.h>
 
 #define RATE  ((uint8_t) 2)
 #define FRAME ((uint8_t) 3)
 #define CLEAR ((uint8_t) 4)
 #define LINE  ((uint8_t) 5)
+#define BOX   ((uint8_t) 6)
+
+int setup(const char* device);
+
+class Display
+{
+	int port;
+public:
+	Display(int port) : port(port) {}
+	void frame()
+	{
+		uint8_t data[] = {FRAME, 0};
+		write(port, data, 1);
+	};
+	void clear()
+	{
+		uint8_t data[] = {CLEAR, 0};
+		write(port, data, 1);
+	}
+	void line(int x1, int y1, int x2, int y2)
+	{
+		uint8_t data[] = {LINE, x1, y1, x2, y2};
+		write(port, data, 5);
+	}
+	void box(int x1, int y1, int x2, int y2)
+	{
+		uint8_t data[] = {BOX, x1, y1, x2, y2};
+		write(port, data, 5);
+	}
+};
+
 
 int main()
 {
-	int port = open("/dev/ttyACM1", O_RDWR | O_NOCTTY | O_NDELAY);
+	// Init serial
+	int port = setup("/dev/ttyACM1");
 	if (port == -1 )
 	{
 		std::cerr << "Could not open port" << std::endl;
 		return 1;
 	}
+	Display display(port);
+	
+	sleep(1);
+	//display.clear();
+	//display.frame();
+	//sleep(1);
+	display.box(0, 0, 31, 7);
+	//sleep(1);
+	//display.frame();
+	//sleep(1);
+	display.line(1, 4, 30, 4);
+	//display.frame();
+	sleep(1);
+	/*for (uint8_t i = 1; i < 8*4; ++i)
+	{
+		display.clear();
+		display.box(0, 0, 32 - 1, 8 - 1);
+		display.line(1, 4, i, 4);
+		display.frame();
+		sleep(0.1);
+	}*/
+	
+	return 0;
+}
+
+int setup(const char* device)
+{
+	// Init serial
+	int port = open(device, O_RDWR | O_NOCTTY | O_NDELAY);
+	if (port == -1 ) return port;
+	
 	fcntl(port, F_SETFL,0);
-	
-	
-	
 	struct termios port_settings;      // structure to store the port settings inc
 	cfsetispeed(&port_settings, B9600);    // set baud rates
 	cfsetospeed(&port_settings, B9600);
-
 	port_settings.c_cflag &= ~PARENB;    // set no parity, stop bits, data bits
 	port_settings.c_cflag &= ~CSTOPB;
 	port_settings.c_cflag &= ~CSIZE;
 	port_settings.c_cflag |= CS8;
-
 	tcsetattr(port, TCSANOW, &port_settings);    // apply the settings to the port
 	
-	uint8_t data[] = {LINE, 0, 0, 16, 0,FRAME};
-	write(port,data,sizeof(data));
-	
-	return 0;
+	return port;
 }
+
